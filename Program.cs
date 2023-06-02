579dbd316df296fbcd33d439d24e0ab9de020f7a
irgendeinDave <zollner.david010306@gmail.com>
2023-06-02 09:12:37 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Welcome to PowerlessShell!");
+        while (true)
+        {
+            Process.Start(Console.ReadLine());
+        }
+    }
+}

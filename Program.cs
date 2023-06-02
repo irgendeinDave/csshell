f0fc286e29d7b5a2459ea3b3da632011c9015a3a
irgendeinDave <zollner.david010306@gmail.com>
2023-06-02 09:50:51 +0200

    added a promt, handle exceptions

--- Program.cs
+++ Program.cs
@@ -1,13 +1,31 @@
 ﻿using System.Diagnostics;
+using System.Text;
 
 public class Program
 {
+    private static string promt = "$ ";
+    private static string startPath;
     public static void Main(String[] args)
     {
-        Console.WriteLine("Welcome to PowerlessShell!");
+
+        Console.WriteLine("Welcome to CsShell!");
         while (true)
         {
-            Process.Start(Console.ReadLine());
+            Console.Write(promt);
+            string? input = Console.ReadLine();
+            if (String.IsNullOrEmpty(input))
+                continue;
+
+
+
+            try
+            {
+                Process.Start(input);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{input} could not be executed: {e.Message}");
+            }
         }
     }
 }

0f2657d7679c429517f3e4410aa69269b8898863
irgendeinDave <zollner.david010306@gmail.com>
2023-06-07 21:11:15 +0200

    move execution of commands into its own class

---
+++ Command.cs
@@ -0,0 +1,85 @@
+using System.Diagnostics;
+namespace cmd;
+
+struct Command
+{
+    public string CommandName;
+    public string Arguments;
+
+    public Command(string cmdName, string args)
+    {
+        CommandName = cmdName;
+        Arguments = args;
+    }
+}
+
+public class CommandRunner
+{
+    public List<String> builtInCommands = new List<String> { "cd", "exit" };
+    public void run(string fullCommand)
+    {
+        run(split(fullCommand));
+    }
+
+    private void run(Command command)
+    {
+        if (isBuildIn(command))
+        {
+            executeBuiltInCommand(command);
+            return;
+        }
+        try
+        {
+            ProcessStartInfo psi = new ProcessStartInfo(command.CommandName);
+            psi.Arguments = command.Arguments;
+            Process runningCommand = Process.Start(psi);
+            runningCommand.WaitForExit();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"{command.CommandName} could not be executed: {e.Message}");
+        }
+    }
+
+    // split the command into program name and argument
+    private Command split(string fullCommand)
+    {
+        int spacePosition = fullCommand.IndexOf(' ');
+        if (spacePosition == -1)
+            return new Command(fullCommand, "");
+        else if (spacePosition == fullCommand.Length - 1)
+            return new Command(fullCommand, "");
+
+        string cmd = fullCommand.Substring(0, spacePosition);
+        string args = fullCommand.Substring(spacePosition + 1);
+        return new Command(cmd, args);
+    }
+
+    private bool isBuildIn(Command command)
+    {
+        foreach (string cmd in builtInCommands)
+        {
+            if (cmd == command.CommandName)
+                return true;
+        }
+        return false;
+    }
+
+    private void executeBuiltInCommand(Command command)
+    {
+        if (command.CommandName == "exit")
+            Environment.Exit(0);
+        else if (command.CommandName == "cd")
+            try
+            {
+                if (command.Arguments == "")
+                    Directory.SetCurrentDirectory(Environment.GetEnvironmentVariable("HOME"));
+                else
+                    Directory.SetCurrentDirectory(command.Arguments);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
-﻿using System.Diagnostics;
+﻿using cmd;
 using InputReader;
 
 public class Program
 {
     private static string prompt = "$ ";
+    private static CommandRunner cr = new CommandRunner();
 
     public static void Main(String[] args)
     {
@@ -19,70 +20,7 @@ public class Program
             if (String.IsNullOrEmpty(input))
                 continue;
 
-            // split input to get arguments
-            int firstSpace = input.IndexOf(' ');
-            string programName;
-            string arguments = "";
-            if (firstSpace != -1)
-            {
-                arguments = input.Substring(firstSpace, input.Length - firstSpace);
-                programName = input.Substring(0, firstSpace);
-            }
-            else
-            {
-                arguments = "";
-                programName = input;
-            }
-
-            // check if command is a built in command and do not run it if it is
-            if (executeBuiltInCommands(input)) continue;
-
-            try
-            {
-                ProcessStartInfo psi = new ProcessStartInfo(programName);
-                psi.Arguments = arguments;
-                Process.Start(psi).WaitForExit();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"{programName} could not be executed: {e.Message}");
-            }
+            cr.run(input);
         }
-    }
-
-    // Parse built-ins and returns if the command is a built in command
-    private static bool executeBuiltInCommands(string input)
-    {
-        if (input == "exit")
-        {
-            Environment.Exit(0);
-            return true; // this is not really necessary but the compiler insists on it
-        }
-        else if (substringUntilChar(input, ' ') == "cd")
-        {
-            Console.WriteLine(input.Substring(2, input.Length - 2));
-            try
-            {
-                if (input.Substring(2) == "")
-                    Directory.SetCurrentDirectory(Environment.GetEnvironmentVariable("HOME"));
-                else
-                    Directory.SetCurrentDirectory(input.Substring(3));
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            return true;
-        }
-        else return false;
-    }
-
-    private static string substringUntilChar(string input, char end)
-    {
-        int endIndex = input.IndexOf(end);
-        if (endIndex != -1)
-            return input.Substring(0, endIndex);
-        else return input;
     }
 }

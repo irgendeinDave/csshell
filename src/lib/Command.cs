6132b95de38b71cbbcbbd320006d056f756f0e7f
david <zollner.david010306@gmail.com>
2023-12-23 21:27:05 +0100

    move the changing of the commands before the're executed to a different class

--- src/lib/Command.cs
+++ src/lib/Command.cs
@@ -1,7 +1,6 @@
 using System.Diagnostics;
-using Aliases;
 using CommandHistory;
-using BuiltInCommands;
+using CommandProcessing;
 
 namespace CommandExecution;
 
@@ -19,59 +18,22 @@ public struct Command
 
 public class CommandRunner
 {
-    private static AliasManager am = new();
-    private BuiltIn bi = new(am);
     public void runLine(string fullCommand)
     {
-        List<string> commands = fullCommand.Trim(';').Split(';').ToList();
-        foreach (string commandSplit in commands)
+        List<Command> processedCommand = CommandProcessor.PreProcess(fullCommand);
+        foreach (Command command in processedCommand)
         {
-            string separatedCommand = commandSplit.Trim();
-            // ignore comments
-            if (separatedCommand.StartsWith("#") || separatedCommand.StartsWith("//"))
-                return;
-
-            // replace alias keywords with the actual commands
-            separatedCommand = am.applyAliases(separatedCommand);
-
-            // do not run empty commands
-            if (separatedCommand.Trim() == string.Empty)
-                return;
-
-            // apply !! operator
-            int doubleExclamationMarkPos = separatedCommand.IndexOf("!!");
-            if (doubleExclamationMarkPos > -1)
-            {
-                if (doubleExclamationMarkPos < separatedCommand.Length - 2)
-                {
-                    if (int.TryParse(new ReadOnlySpan<char>(separatedCommand[doubleExclamationMarkPos + 2]), out int index))
-                    {
-                        separatedCommand = separatedCommand.Replace($"!!{index}", History.StoredCommand(index));
-                    }
-                } // No index given and !! at the end of the command
-                else
-                    separatedCommand = separatedCommand.Replace($"!!", History.StoredCommand(0));
-            }
-        
-            // execute the command
-            Command command = split(separatedCommand);
-            command.Arguments = processArguments(command);
-            if (isBuiltIn(command))
-            {
-                bi.executeBuiltInCommand(command);
-                return;
-            }
-
+            string combined = $"{command.CommandName} {command.Arguments}";
             int exitCode;
             // pipes
             if (fullCommand.Contains('|'))
             {
-                string firstCommand = separatedCommand[..separatedCommand.IndexOf('|')].Trim();
-                string secondCommand = separatedCommand[(separatedCommand.IndexOf('|') + 1)..].Trim();
+                string firstCommand = combined[..combined.IndexOf('|')].Trim();
+                string secondCommand = combined[(combined.IndexOf('|') + 1)..].Trim();
 
-                command = split(firstCommand);
+                Command newcommand = CommandProcessor.split(firstCommand);
 
-                exitCode = execute(command, out string output, out string error);
+                exitCode = execute(newcommand, out string output, out string error);
                 if (error != string.Empty)
                 {
                     Console.Write(error);
@@ -79,20 +41,20 @@ public class CommandRunner
                     continue;
                 }
 
-                exitCode = execute(split($"{secondCommand} {output}"));
+                exitCode = execute(CommandProcessor.split($"{secondCommand} {output}"));
                 Environment.SetEnvironmentVariable("?", exitCode.ToString());
                 if (exitCode == 0)
-                    History.Append(separatedCommand);
+                    History.Append(combined);
                 continue;
             }
 
             exitCode = execute(command);
             if (exitCode == 0)
-                History.Append(separatedCommand);
+                History.Append(combined);
         }
     }
 
-    private readonly List<string> builtInCommands = new() { "cd", "exit", "set", "alias" };
+
 
     private int execute(Command command)
     {
@@ -142,95 +104,13 @@ public class CommandRunner
             return 1;
         }
     }
-    
-    // split the command into program name and arguments
-    private Command split(string fullCommand)
-    {
-        int spacePosition = fullCommand.IndexOf(' ');
-        if (spacePosition == -1)
-            return new Command(fullCommand, "");
-        else if (spacePosition == fullCommand.Length - 1)
-            return new Command(fullCommand, "");
-
-        string cmd = fullCommand[..spacePosition];
-        string args = fullCommand[(spacePosition + 1)..];
-        return new Command(cmd, args);
-    }
-
-    private bool isBuiltIn(Command command)
-    {
-        foreach (string cmd in builtInCommands)
-        {
-            if (cmd == command.CommandName)
-                return true;
-        }
-        return false;
-    }
-
-    /// <summary>
-    /// replace variables with their values and path modifiers like ~ with the full paths
-    /// </summary>
-    /// <param name="command"> The original command </param>
-    /// <returns> the new arguments for the command </returns>
-    private string processArguments(Command command)
-    {
-        string args = String.Empty;
-        List<string> split = Util.splitCommand(command.Arguments);
-
-        // variables
-        foreach (string arg in split)
-        {
-            if (arg == "")
-                break;
-            
-            // two dollar signs can be used to give the range of a variable name
-            if (arg.Contains('$'))
-            {
-                 
-                int dsPosition = arg.IndexOf('$'); // can't be -1 because of the if statement above
-                int secondDs = arg.Substring(dsPosition + 1).IndexOf('$') + dsPosition;
-                string? value;
-                if (secondDs == -1)
-                    value = Environment.GetEnvironmentVariable(arg.Substring(dsPosition + 1));
-                else
-                    value = Environment.GetEnvironmentVariable(arg.Substring(dsPosition + 1, secondDs - dsPosition));
-                string result = arg.Substring(0, dsPosition);
-                if (value == null)
-                {
-                    Console.WriteLine("Value not found!");
-                }
-                else
-                {
-                    result += value;
-                    if (secondDs > -1)
-                        result += arg.Substring(secondDs + 2);
-                    result = result.Replace(" ", null);
-                    args += result; 
-                }
-            }
-            else if (arg.StartsWith("~"))
-            {
-                args += arg;
-                args = args.Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
-            }
-            else if (arg.StartsWith("!!"))
-            {
-                args += arg;
-                if (arg.Length == 2)
-                    args = args.Replace("!!", History.StoredCommand(0));
-                else
-                {
-                    if (int.TryParse(arg.Substring(2), out int index))
-                    {
-                        args = args.Replace("!!" + index, History.StoredCommand(index));
-                    }
-                }
-            }
-            else
-            {
-                args += $"{arg} ";
-            }
-        }
-        return args;
-    }
 }
+
+
+
+
+    
+
+    
+
+    
---
+++ src/lib/CommandProcessor.cs
@@ -0,0 +1,155 @@
+using CommandExecution;
+using BuiltInCommands;
+using Aliases;
+using CommandHistory;
+
+namespace CommandProcessing;
+
+public static class CommandProcessor
+{
+    private static AliasManager am = new();
+    private static BuiltIn bi = new(am);
+    private static readonly List<string> builtInCommands = new() { "cd", "exit", "set", "alias" };
+    
+    /// <summary>
+    /// Get a list of commands that can be executed as they are
+    /// ignores pipes
+    /// </summary>
+    /// <param name="fullCommand">the full line as it is typed in by the user</param>
+    /// <returns>a list of commands ready to run as objects of the Command struct</returns> 
+    public static List<Command> PreProcess(string fullCommand)
+    {
+        List<Command> commands = new();
+        List<string> commandString = fullCommand.Trim(';').Split(';').ToList();
+        foreach (string commandSplit in commandString)
+        {
+            string separatedCommand = commandSplit.Trim();
+            // ignore comments
+            if (separatedCommand.StartsWith("#") || separatedCommand.StartsWith("//"))
+                break;
+
+            // replace alias keywords with the actual commands
+            separatedCommand = am.applyAliases(separatedCommand);
+
+            // do not run empty commands
+            if (separatedCommand.Trim() == string.Empty)
+                return new();
+
+            // apply !! operator
+            int doubleExclamationMarkPos = separatedCommand.IndexOf("!!");
+            if (doubleExclamationMarkPos > -1)
+            {
+                if (doubleExclamationMarkPos < separatedCommand.Length - 2)
+                {
+                    if (int.TryParse(new ReadOnlySpan<char>(separatedCommand[doubleExclamationMarkPos + 2]),
+                            out int index))
+                    {
+                        separatedCommand = separatedCommand.Replace($"!!{index}", History.StoredCommand(index));
+                    }
+                } // No index given and !! at the end of the command
+                else
+                    separatedCommand = separatedCommand.Replace($"!!", History.StoredCommand(0));
+            }
+            Command command = split(separatedCommand);
+            command.Arguments = processArguments(command);
+            if (isBuiltIn(command))
+            {
+                bi.executeBuiltInCommand(command);
+                continue; // built in commands should not be added to the list and therefore skipped
+            }
+            commands.Add(command);
+        }
+        return commands;
+    }
+    
+    // split the command into program name and arguments
+    public static Command split(string fullCommand)
+    {
+        int spacePosition = fullCommand.IndexOf(' ');
+        if (spacePosition == -1)
+            return new Command(fullCommand, "");
+        else if (spacePosition == fullCommand.Length - 1)
+            return new Command(fullCommand, "");
+
+        string cmd = fullCommand[..spacePosition];
+        string args = fullCommand[(spacePosition + 1)..];
+        return new Command(cmd, args);
+    }
+    
+    /// <summary>
+    /// replace variables with their values and path modifiers like ~ with the full paths
+    /// </summary>
+    /// <param name="command"> The original command </param>
+    /// <returns> the new arguments for the command </returns>
+    private static string processArguments(Command command)
+    {
+        string args = String.Empty;
+        List<string> split = Util.splitCommand(command.Arguments);
+
+        // variables
+        foreach (string arg in split)
+        {
+            if (arg == "")
+                break;
+            
+            // two dollar signs can be used to give the range of a variable name
+            if (arg.Contains('$'))
+            {
+                 
+                int dsPosition = arg.IndexOf('$'); // can't be -1 because of the if statement above
+                int secondDs = arg.Substring(dsPosition + 1).IndexOf('$') + dsPosition;
+                string? value;
+                if (secondDs == -1)
+                    value = Environment.GetEnvironmentVariable(arg.Substring(dsPosition + 1));
+                else
+                    value = Environment.GetEnvironmentVariable(arg.Substring(dsPosition + 1, secondDs - dsPosition));
+                string result = arg.Substring(0, dsPosition);
+                if (value == null)
+                {
+                    Console.WriteLine("Value not found!");
+                }
+                else
+                {
+                    result += value;
+                    if (secondDs > -1)
+                        result += arg.Substring(secondDs + 2);
+                    result = result.Replace(" ", null);
+                    args += result; 
+                }
+            }
+            else if (arg.StartsWith("~"))
+            {
+                args += arg;
+                args = args.Replace("~", Environment.GetFolderPath(Environment.SpecialFolder.UserProfile));
+            }
+            else if (arg.StartsWith("!!"))
+            {
+                args += arg;
+                if (arg.Length == 2)
+                    args = args.Replace("!!", History.StoredCommand(0));
+                else
+                {
+                    if (int.TryParse(arg.Substring(2), out int index))
+                    {
+                        args = args.Replace("!!" + index, History.StoredCommand(index));
+                    }
+                }
+            }
+            else
+            {
+                args += $"{arg} ";
+            }
+        }
+        return args;
+    }
+    
+    private static bool isBuiltIn(Command command)
+    {
+        foreach (string cmd in builtInCommands)
+        {
+            if (cmd == command.CommandName)
+                return true;
+        }
+        return false;
+    }
+}

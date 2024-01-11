ba0cc7fa8175a12b79983450061ef84eed99a793
david <zollner.david010306@gmail.com>
2024-01-11 20:34:56 +0100

    parse script into packages

--- src/lib/Script.cs
+++ src/lib/Script.cs
@@ -2,15 +2,43 @@ using CommandExecution;
 
 public static class Script
 {
+    private static Dictionary<string, List<string>> commandPackages = new();
     public static void runScript(string path, CommandRunner cr)
     {
         string[] lines = File.ReadAllLines(path);
-        foreach (string line in lines)
-        {
-            cr.runLine(line);
-        }
+        ParseFile(lines);
+        //TODO: Run commands
 
         if (path != Settings.RcFilePath)
             Environment.Exit(0);
+    }
+
+    private static void ParseFile(string[] fileContent)
+    {
+        List<string> currentPackage = new();
+        
+        for(int i = 0; i < fileContent.Length; ++i)
+        {
+            // functions
+            if (fileContent[i].StartsWith("fn "))
+            {
+                //TODO: Test and maybe turn this into a method
+                string packageName = getFunctionName(fileContent[i]);
+                ++i;
+                int count = 0;
+                while (fileContent[i] != "end")
+                {
+                    currentPackage.Add(fileContent[i]);
+                    ++i;
+                }
+                ++i;
+                commandPackages.Add(getFunctionName(packageName), currentPackage);
+            }
+        }
+    }
+
+    private static string getFunctionName(string line)
+    {
+        return line.Substring(3);
     }
 }
